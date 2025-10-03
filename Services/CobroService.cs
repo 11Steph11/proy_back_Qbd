9b0ab2857e57a01cf2185d0e65a4ae056445e3a3
4DNB <davidnb03@outlook.com>
2025-10-03 17:57:05 -0500

    c

--- Services/CobroService.cs
+++ Services/CobroService.cs
@@ -73,6 +73,7 @@ namespace Proy_back_QBD.Services
             {
                 return null;
             }
+
             decimal? totalPedido = 0;
             decimal? totalCobro = 0;
 
@@ -93,10 +94,13 @@ namespace Proy_back_QBD.Services
 
             Cobro cobro = _mapper.Map<Cobro>(request);
             cobro.ModificadorId = cobro.CreadorId;
+            pedido.Adelanto = totalCobro;
+            pedido.Saldo = totalPedido + totalCobro;
             await _context.Cobros.AddAsync(cobro);
             await _context.SaveChangesAsync();
             cobroCreateRes.Cobro = cobro;
             return cobroCreateRes;
+
         }
 
         // public async Task<List<CobroFindAllResponse?>> Obtener()
