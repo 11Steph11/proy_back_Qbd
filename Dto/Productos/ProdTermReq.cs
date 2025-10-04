9ff7e1d4d51c981f58527928e993dae95c75ff8a
ghelbert <gustavo_reinoso@usmp.pe>
2025-10-04 11:57:25 -0500

    w

--- Dto/Productos/ProdTermReq.cs
+++ Dto/Productos/ProdTermReq.cs
@@ -9,7 +9,6 @@ namespace Proy_back_QBD.Dto.Request
         public int? ProductoId { get; set; }                     // Cantidad de unidades solicitadas
         public string? ZonaAplicacion { get; set; }              // Zona donde se aplica el tratamiento (si aplica)
         public string? Diagnostico { get; set; }               // Diagnóstico relacionado al pedido
-        public string? Estado { get; set; }                    // Estado del pedido (pendiente, procesado, entregado, etc.)
         public int CreadorId { get; set; }
     }
     public class ProdTermCreateReq
