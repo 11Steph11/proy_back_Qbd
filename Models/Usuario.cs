8a7ca977fb4f4bb4647b70944e2b405eadf32c59
4DNB <davidnb03@outlook.com>
2025-10-05 19:26:24 -0500

    cod login

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -46,6 +46,7 @@ namespace Proy_back_QBD.Models
         public string? Cmp { get; set; }  // Puede ser nulo
         [Column("sedeId")]
         public int? SedeId { get; set; }  // Puede ser nulo
+        [JsonIgnore]
         public Sede? Sede { get; set; }  // Puede ser nulo
         [Column("horario_almuerzo")]
         public TimeOnly? HorarioAlmuerzo { get; set; }  // Puede ser nulo
@@ -53,33 +54,61 @@ namespace Proy_back_QBD.Models
         public TimeOnly? HorarioRegreso { get; set; }  // Puede ser nulo
         [Column("codigo")]
         public string? Codigo { get; set; }
+        [JsonIgnore]
         public List<Asistencia>? AsistenciasCreadas { get; set; }
+        [JsonIgnore]
         public List<Asistencia>? AsistenciasModificadas { get; set; }
+        [JsonIgnore]
         public List<Especialidad>? EspecialidadsCreadas { get; set; }
+        [JsonIgnore]
         public List<Especialidad>? EspecialidadsModificadas { get; set; }
+        [JsonIgnore]
         public List<Formula>? FormulasCreadas { get; set; }
+        [JsonIgnore]
         public List<Formula>? FormulasModificadas { get; set; }
+        [JsonIgnore]
         public List<Medico>? MedicosCreadas { get; set; }
+        [JsonIgnore]
         public List<Medico>? MedicosModificadas { get; set; }
+        [JsonIgnore]
         public List<Paciente>? PacientesCreadas { get; set; }
+        [JsonIgnore]
         public List<Paciente>? PacientesModificadas { get; set; }
+        [JsonIgnore]
         public List<Pedido>? PedidosCreadas { get; set; }
+        [JsonIgnore]
         public List<Pedido>? PedidosModificadas { get; set; }
+        [JsonIgnore]
         public List<Persona>? PersonasCreadas { get; set; }
+        [JsonIgnore]
         public List<Persona>? PersonasModificadas { get; set; }
+        [JsonIgnore]
         public List<Sede>? SedesCreadas { get; set; }
+        [JsonIgnore]
         public List<Sede>? SedesModificadas { get; set; }
+        [JsonIgnore]
         public List<Usuario>? UsuariosCreadas { get; set; }
+        [JsonIgnore]
         public List<Usuario>? UsuariosModificadas { get; set; }
+        [JsonIgnore]
         public List<TipoUsuario>? TUCreadas { get; set; }
+        [JsonIgnore]
         public List<TipoUsuario>? TUModificadas { get; set; }
+        [JsonIgnore]
         public List<ProdTerm>? PTCreados { get; set; }
+        [JsonIgnore]
         public List<ProdTerm>? PTModificados { get; set; }
+        [JsonIgnore]
         public List<Cobro>? CobrosCreadas { get; set; }
+        [JsonIgnore]
         public List<Cobro>? CobrosModificadas { get; set; }
+        [JsonIgnore]
         public List<Laboratorio>? LaboratorioCreadas { get; set; }
+        [JsonIgnore]
         public List<Laboratorio>? LaboratorioModificadas { get; set; }
+        [JsonIgnore]
         public List<Producto>? ProductoCreadas { get; set; }
+        [JsonIgnore]
         public List<Producto>? ProductoModificadas { get; set; }
     }
     [Table("tipos_usuario")]
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -17,7 +17,7 @@ namespace Proy_back_QBD.Services
             _context = context;
             _mapper = mapper;
         }
-        public async Task<UsuarioLoginDataRes?> ValidarLogin(string dni, string contrasena)
+        public async Task<UsuarioLoginDataRes?> ValidarLogin(string codigo, string contrasena)
         {
 
 
@@ -25,7 +25,7 @@ namespace Proy_back_QBD.Services
             .Include(a => a.Persona)
             .Include(a => a.Sede)
             .Include(a => a.Tipo)
-            .Where(a => a.Persona.Dni.Trim() == dni && a.Contrasena.Trim() == contrasena)
+            .Where(a => a.Codigo.Trim() == codigo && a.Contrasena.Trim() == contrasena)
             .Select(a => new UsuarioLoginDataRes
             {
                 NombreCompleto = $"{a.Persona.NombreCompleto}",
