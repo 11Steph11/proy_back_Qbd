57bd9aa54537d3fa6e2d202be7b84f62df5371cf
ghelbert <gustavo_reinoso@usmp.pe>
2025-10-12 18:32:50 -0500

    r

--- Models/Pedido/PedidoRes.cs
+++ Models/Pedido/PedidoRes.cs
@@ -59,6 +59,7 @@ namespace Proy_back_QBD.Dto.Response
     {
         public int? Id { get; set; }
         public string? Periodo { get; set; }
+        public string? Recibo { get; set; }
         public string? Img1 { get; set; }
         public string? Img2 { get; set; }
         public string? Img3 { get; set; }
